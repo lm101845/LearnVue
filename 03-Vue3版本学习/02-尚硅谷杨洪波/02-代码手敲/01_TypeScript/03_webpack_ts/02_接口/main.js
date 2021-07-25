ecaeac4272d7901fd3d55f2399545ddd82e836cf
lm101845 <1018457683@qq.com>
2021-07-25 19:16:51 +0800

    07-25

--- 03-Vue3版本学习/02-尚硅谷杨洪波/02-代码手敲/01_TypeScript/03_webpack_ts/02_接口/main.js
+++ 03-Vue3版本学习/02-尚硅谷杨洪波/02-代码手敲/01_TypeScript/03_webpack_ts/02_接口/main.js
@@ -1,15 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /*
  * @Author: liming
  * @Date: 2021-07-12 15:30:01
- * @LastEditTime: 2021-07-25 10:55:32
+ * @LastEditTime: 2021-07-25 09:20:15
  * @FilePath: \02-尚硅谷杨洪波\02-代码手敲\01_TypeScript\03_webpack_ts\src\main.ts
  */
 // import './01_基础类型'
 // import './01_接口'
 // import './02_函数类型'
-// import './03_类类型'
-// import './01_类'
-// import './02_继承'
-// import './03_多态'
-import './04_修饰符'
-document.write('哈哈，我又变帅了')
+require("./03_\u7C7B\u7C7B\u578B");
+document.write('哈哈，我又变帅了');
--- 03-Vue3版本学习/02-尚硅谷杨洪波/02-代码手敲/01_TypeScript/TypeScript简略笔记.md
+++ 03-Vue3版本学习/02-尚硅谷杨洪波/02-代码手敲/01_TypeScript/TypeScript简略笔记.md
@@ -1,5 +1,11 @@
+<!--
+ * @Author: liming
+ * @Date: 2021-07-08 16:16:03
+ * @LastEditTime: 2021-07-25 09:44:48
+ * @FilePath: \02-尚硅谷杨洪波\02-代码手敲\01_TypeScript\TypeScript简略笔记.md
+-->
 # TypeScript简略笔记
 
 * 输入`tsc --init`可以得到`tsconfig.json`配置文件
-* 
+* 输入`npm run dev`可以使用webpack打开ts
 

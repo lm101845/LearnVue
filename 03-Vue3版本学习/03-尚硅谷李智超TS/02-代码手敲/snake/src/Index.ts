28bcadcbc167af91251780b1112b83b2fd7e91ce
lm101845 <1018457683@qq.com>
2021-08-07 07:42:43 +0800

    Auto Commit.

---
+++ 03-Vue3版本学习/03-尚硅谷李智超TS/02-代码手敲/snake/src/Index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: liming
+ * @Date: 2021-08-07 05:05:50
+ * @LastEditTime: 2021-08-07 05:13:02
+ * @FilePath: \03-尚硅谷李智超TS\02-代码手敲\snake\src\Index.ts
+ */
+
+//引入样式
+import './style/index.less'
+console.log(123);

59f791781eef5733e2009c3cce03d3a4179796ed
lm101845 <1018457683@qq.com>
2021-10-10 21:45:00 +0800

    Auto Commit.

--- 06-Vue全家桶复习/01-VueRouter/01-官方文档/01-基础/src/main.js
+++ 06-Vue全家桶复习/01-VueRouter/01-官方文档/01-基础/src/main.js
@@ -1,37 +1,32 @@
 /*
  * @Author: liming
  * @Date: 2021-08-03 09:55:48
- * @LastEditTime: 2021-09-08 19:20:52
- * @FilePath: \03-尚硅谷张天禹(好)\02-代码手敲\vue_test\28_Vue路由基础-复习1遍\main.js
+ * @LastEditTime: 2021-10-10 15:09:01
+ * @FilePath: \01-基础\src\main.js
  */
 
-//引入Vue
-import Vue from 'vue';
-//引入App.vue
+// 引入Vue
+import Vue from 'vue'
+// 引入App.vue
 import App from './App'
 
 // 引入VueRouter——它是一个插件库，需要use一下
-import VueRouter from 'vue-router';
+import VueRouter from 'vue-router'
 
 // 引入路由器(一个应用有一个路由器就够了)
 // 因为是index.js,所以后面的可以不写
-import  router from './router';
-//应用插件
-Vue.use(VueRouter);
+import router from './router'
+// 应用插件
+Vue.use(VueRouter)
 
-
-//关闭Vue的生产提示
+// 关闭Vue的生产提示
 Vue.config.productionTip = false
 
-
-new Vue({
-  el: "#app",
+const app = new Vue({
+  el: '#app',
   render: (h) => h(App),
-//   router: "hello", 
-// 你引用了vue-router之后，它就有一个全新的配置项了
-// 但是这里不能随便写：Error in beforeCreate hook: "TypeError: this._router.init is not a function"
-router,
-});
-
-
- 
+  // router: "hello",
+  // 你引用了vue-router之后，它就有一个全新的配置项了
+  // 但是这里不能随便写：Error in beforeCreate hook: "TypeError: this._router.init is not a function"
+  router
+})
